87b1212e4ad983dbf842619f9d369e2322348334
NS918326 <Navrajgaltee@outlook.com>
2024-02-19 03:44:56 -0500

    Project setup

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,103 @@
+"use client";
+import Image from "next/image";
+import React, { useState, useTransition } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <div className="ml-2 flex flex-row gap-x-10">
+        <div>
+          <ul className="list-disc pl-2">
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Next.js</li>
+            <li>Express</li>
+            <li>PostgreSQL</li>
+          </ul>
+        </div>
+        <div>
+          <ul className="list-disc pl-2">
+            <li>MongoDB</li>
+            <li>Java Spring boot</li>
+            <li>Google Cloud</li>
+            <li>AWS</li>
+            <li>Angular</li>
+          </ul>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Eudcation",
+    id: "education",
+    content: (
+      <ul className="ml-2 list-disc pl-2">
+        <li>
+          Computer System Technology: Software Engineering, Sheridan College
+        </li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          alt="about-image"
+          width={500}
+          height={500}
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base md:text-lg">
+            I am a detail-oriented, responsible, and versatile Full Stack
+            Developer. With over 3 years of expertise in web development, I
+            uphold a technical proficiency in Javascript, Java, Typescript, and
+            popular frameworks such as Node.js, Express, React.js, Next.js,
+            Angular, as well as databases like MySQL and MongoDB. Additionally,
+            I have hands-on experience with Springboot and Ruby for Java web
+            development. I am continuously learning, and staying up-to-date with
+            the trends, and emerging technologies. Also, I acquire a practical
+            knowledge of working with services of Google Cloud and AWS,
+            including the expertise of CI/CD principles.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              active={tab === "skills"}
+              selectTab={() => handleTabChange("skills")}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              active={tab === "education"}
+              selectTab={() => handleTabChange("education")}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import NavLink from "./NavLink";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuOverlay from "./MenuOverlay";
+
+const navLinks = [
+  { title: "About", path: "#about" },
+  { title: "Experience", path: "#experience" },
+  { title: "Projects", path: "#projects" },
+  { title: "Contact", path: "#contact" },
+];
+
+const NavBar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  return (
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-6 py-3">
+        <Link href={"/"}>
+          <Image src="/images/logo.png" alt="logo" width={60} height={60} />
+        </Link>
+        <div className="mobile-menu block md:hidden">
+          {navbarOpen ? (
+            <button
+              onClick={() => setNavbarOpen(false)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white duration-200 ease-in-out"
+            >
+              <CloseIcon />
+            </button>
+          ) : (
+            <button
+              onClick={() => setNavbarOpen(true)}
+              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white duration-200 ease-in-out"
+            >
+              <MenuOutlinedIcon />
+            </button>
+          )}
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:px-6 md:py-5 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
+    </nav>
+  );
+};
+
+export default NavBar;
